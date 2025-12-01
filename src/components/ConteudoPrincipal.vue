80bebf2945f3f72e9b0a01c0761f940f68f7a2d6
Abel Silva <abel2901@gmail.com>
2025-12-01 11:13:14 -0300

    Criando componente selecionar ingredientes

--- src/components/ConteudoPrincipal.vue
+++ src/components/ConteudoPrincipal.vue
@@ -1,10 +1,13 @@
 <script lang="ts">
+import SelecionarIngredientes from './SelecionarIngredientes.vue';
+
 export default {
     data() {
         return {
             ingredientes: ['Alho', 'Manteiga', 'Orégano']
         }
-    }
+    },
+    components: { SelecionarIngredientes }
 }
 </script>
 
@@ -25,8 +28,10 @@ export default {
               <img src="../assets/imagens/icones/lista-vazia.svg" alt="Ícone de pesquisa">
               Sua lista está vazia, selecione ingredientes para iniciar.
             </p>
-
         </section>
+
+        <SelecionarIngredientes />
+
     </main>
 </template>
 
